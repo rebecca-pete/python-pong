25da24e391a9d25db805d0ca81f31079aae862fd
Rebeccca Peterson <rebecca.peterson.soprano@gmail.com>
2020-04-07 00:18:20 -0700

    Update Pong1.py

--- Pong1.py
+++ Pong1.py
@@ -4,7 +4,18 @@
 
 # modules to import:
 import turtle
-import tkinter as TK
+import tkinter 
+
+# from tkinter import *
+# root = Tk()
+# # Creating a Label Widget
+# myLabel = Label(root, text="Hello World!")
+#  # Shoving it onto the screen
+# myLabel.pack()
+# root.mainloop()
+# top = tkinter.Tk()
+# # Code to add widgets goes here...
+# top.mainloop()
 
 wn = turtle.Screen()
 wn.title("Pong by @TokyoEdTech")
@@ -12,21 +23,108 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score 
+score_a = 0
+score_b = 0
+
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A:0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y +=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -=20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y +=20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -=20
+    paddle_b.sety(y)
+    
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 
 while True:
     wn.update()
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+
+    # Paddle collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40)):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40)):
+        ball.setx(-340)
+        ball.dx *= -1
