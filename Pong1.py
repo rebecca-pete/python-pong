4ec56c7eec6f4f9920d8a7b7b7dcf76dc8377478
Rebeccca Peterson <rebecca.peterson.soprano@gmail.com>
2020-04-07 00:52:06 -0700

    Adding score

--- Pong1.py
+++ Pong1.py
@@ -114,11 +114,15 @@ while True:
         ball.goto(0, 0)
         ball.dx *= -1
         score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle collisions
     if (ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40)):
